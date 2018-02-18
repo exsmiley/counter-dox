71dfc9d90bb2eee78ff9b37f7d11b7fc104d3c26
Will Papper <contact@papper.me>
2018-02-17 20:28:28 -0800

    Notifications are beautiful

--- mobileApp/components/Notification.js
+++ mobileApp/components/Notification.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Fire from '../api/Fire'
 import * as firebase from 'firebase'
+import StatusBarAlert from 'react-native-statusbar-alert';
+
 const _ = require("lodash");
 
 import {
@@ -38,23 +40,15 @@ export default class Notification extends React.Component {
   }
 
   render() {
+    console.warn("notification")
     return (
       <View style={styles.container}>
-        <Text style={{ fontSize: 15 }}>{this.props.title}</Text>
-        <Text style={{ fontSize: 12 }}>{this.props.subtitle}</Text>
-        <View style={styles.buttonContainer}>
-            <Button
-                style={styles.button}
-                title={"Yes"}
-                onPress={this.props.pressedYes}
-            />
-            <View style={styles.spacer} />
-            <Button
-                style={styles.button}
-                onPress={this.props.pressedNo}
-                title={"No"}
-            />
-        </View>
+        <StatusBarAlert
+        visible={true}
+        message="Dox detected! Would you like to counter dox?"
+        style={styles.alert}
+        onPress={() => this.navigator.push({id: 'SilentAlert'})}
+        />
       </View>
     );
   }
@@ -72,60 +66,21 @@ export default class Notification extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    height: 84,
-    flexDirection: "column",
-    alignItems: "stretch",
-    backgroundColor: 'rgba(255, 255, 255, 0.9)',
-    paddingTop: 25,
-    paddingLeft: 10,
-    paddingRight: 10,
-    marginBottom: 50,
-    borderBottomWidth: 1,
-    borderBottomColor: "rgba(125, 125, 125, 0.5)",
-  },
-  buttonContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "center",
-    display: "flex",
-    marginBottom: 10,
-  },
-  spacer: {
-    flex: 1,
-  },
-  button: {
-    backgroundColor: "#000",
-    flex: 1,
-    padding: 10,
-    borderRadius: 2,
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  flatList: {
-    flex: 1,
-  },
-  item: {
-    flex: 1,
-    flexDirection: "column",
-    alignItems: "flex-start",
-    paddingLeft: 10,
-  },
-  itemName: {
-      fontSize: 15,
-      color: "#000",
-  },
-  itemCity: {
-    fontSize: 12,
-    color: "#555",
-  },
+    container: {
+      ...StyleSheet.absoluteFillObject,
+      alignSelf: 'flex-end',
+      marginTop: 50,
+      height: 30,
+    },
+    alert: {
+        backgroundColor: "#3CC29E",
+        color: "white"
+    },
+    content: {
+      flex: 1,
+      alignItems: 'center',
+    },
+    text: {
+      textAlign: 'center'
+    }
 });
